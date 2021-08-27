88326113f0bbb668ca904e2a6984bb46c1d7a672
alalalalaki <harlan.zhu@gmail.com>
2021-08-28 05:56:21 +0900

    Fix build and publish issues

--- setup.py
+++ setup.py
@@ -86,11 +86,8 @@ setup_args = dict(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Framework :: Jupyter",
-        "Framework :: Jupyter :: JupyterLab :: 3",
-        "Framework :: Jupyter :: JupyterLab :: Extensions",
-        "Framework :: Jupyter :: JupyterLab :: Extensions :: Prebuilt",
-        "Framework :: Jupyter :: JupyterLab :: Extensions :: Themes",
     ],
 )
 
